9c3c3ca2f26c89e17788d34f1b5ca1dabf0332a4
kartik-madhak <codebull707@gmail.com>
2022-04-14 00:04:31 +0530

    ADD: game of life init

---
+++ test/testGameOfLife.js
@@ -0,0 +1,13 @@
+// Requiring module
+const assert = require('assert');
+
+// We can group similar tests inside a describe block
+describe("Game Of Life", () => {
+
+    let gameOfLife;
+
+    before(() => {
+        gameOfLife = new GameOfLife;
+    });
+
+});
